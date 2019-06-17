606476c80b4c59dd1503ae1d9d6f46b367876285
Asmaathabet <asmathabet2009@hotmail.com>
2019-06-17 11:32:40 +0300

    add buttons

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ var msHundredsLabel = document.getElementById("msHundreds");
 var msTensLabel = document.getElementById("msTens");
 var digitsDiv = document.querySelector('.digits');
 
-var totalMilliSeconds = 0;
-setInterval(setTime, 1000);
+// Add start button 
+var startButton = document.createElement("BUTTON");
+startButton.innerHTML = 'Start'; 
+startButton.style.height = '50px';
+startButton.style.width = '50px';
+startButton.style.margin = '20px';
+digitsDiv.appendChild(startButton); 
+
+// Add Reset button 
+var resetButton = document.createElement("BUTTON");
+resetButton.innerHTML = 'Reset'; 
+resetButton.style.height = '50px';
+resetButton.style.width = '50px';
+resetButton.style.margin = '20px';
+digitsDiv.appendChild(resetButton);
+
+// Add Stop button 
+var stopButton = document.createElement("BUTTON");
+stopButton.innerHTML = 'Stop'; 
+stopButton.style.height = '50px';
+stopButton.style.width = '50px';
+stopButton.style.margin = '20px';
+digitsDiv.appendChild(stopButton);
+
+
+startButton.addEventListener('click', () => {
+    var totalMilliSeconds = 0;
+    intervalId = setInterval(setTime, 1000);
 
 function setTime() {
   ++totalMilliSeconds;
@@ -15,11 +41,9 @@ function setTime() {
   secondsOnesLabel.innerHTML = pad(parseInt(totalMilliSeconds / 10)%10);
   secondsTensLabel.innerHTML = 0;
   
-  digitsDiv.style.color = (parseInt(totalMilliSeconds / 10)%10  == 0)? 'red':'black'; 
+  digitsDiv.style.color = (parseInt(totalMilliSeconds / 10)%10  == 0 && totalMilliSeconds % 10 == 0)? 'red':'black'; 
   
 }
-console.log(secondsOnesLabel);
-console.log(secondsTensLabel);
 
 function pad(val){
     var valString = val + "";
@@ -32,3 +56,20 @@ function pad(val){
         return valString;
     }
 } 
+
+  });
+
+
+stopButton.addEventListener('click', () => {
+    if (intervalId)
+      clearInterval(intervalId);
+  });
+  
+resetButton.addEventListener('click', () => {
+    totalMilliSeconds = 0;
+    document.getElementById("secondTens").innerHTML= '0';
+    document.getElementById("secondTens").innerHTML= '0';
+    document.getElementById("msHundreds").innerHTML= '0';
+    document.getElementById("msTens").innerHTML= '0';
+    
+ });
